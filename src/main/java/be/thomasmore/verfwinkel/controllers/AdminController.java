0b6fb368cdeb9090cf2970b9f13daf9540ab9688
Jeroen Van Hees <r0896556@student.thomasmore.be>
2022-04-10 15:14:22 +0200

    verf aanpassen

--- src/main/java/be/thomasmore/verfwinkel/controllers/AdminController.java
+++ src/main/java/be/thomasmore/verfwinkel/controllers/AdminController.java
@@ -1,21 +1,60 @@
 package be.thomasmore.verfwinkel.controllers;
+import be.thomasmore.verfwinkel.model.Verfpot;
+import be.thomasmore.verfwinkel.repositories.VerfpotRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
+import java.util.Optional;
 
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
     private Logger logger = LoggerFactory.getLogger(AdminController.class);
-    @GetMapping({"/admin/admin"})
+    @Autowired
+    private VerfpotRepository verfpotRepository;
+    @GetMapping({"/admin"})
     public String admin(Model model, Principal principal) {
         final String loginName = principal==null ? "NOBODY" : principal.getName();
         logger.info("admin - logged in as " + loginName);
         model.addAttribute("principal", principal);
         return "admin/admin";
+    }
+
+    @GetMapping("/verfAanpassen/{id}")
+    public String verfAanpassen(Model model, @PathVariable int id) {
+        logger.info("verfAanpassen"+id);
+        Optional<Verfpot> optionalVerfpot = verfpotRepository.findById(id);
+        if (optionalVerfpot.isPresent()) {
+            model.addAttribute("verfpot", optionalVerfpot.get());
+        }
+        return "admin/verfAanpassen";
+    }
+
+    @PostMapping("/verfAanpassen/{id}")
+    public String verfEditPost(Model model, @PathVariable int id,
+                               @ModelAttribute("verfpot") Verfpot verfpot) {
+        logger.info("verdeditpost : " + id + " -- new name: " + verfpot.getNaam());
+        logger.info("verdeditpost : " + id + " -- new og: " + verfpot.getOndergrond());
+        logger.info("verdeditpost : " + id + " -- new kleur: " + verfpot.getKleurAanbod());
+        logger.info("verdeditpost : " + id + " -- new vp: " + verfpot.getVerpakkingMogelijkheden());
+        logger.info("verdeditpost : " + id + " -- new prijs: " + verfpot.getPrijs());
+
+        Optional<Verfpot> optionalVerfpot = verfpotRepository.findById(id);
+        if (optionalVerfpot.isPresent()) {
+            Verfpot verfpotVoorUpdate = optionalVerfpot.get();
+            verfpotVoorUpdate.setNaam(verfpot.getNaam());
+            verfpotVoorUpdate.setOndergrond(verfpot.getOndergrond());
+            verfpotVoorUpdate.setKleurAanbod(verfpot.getKleurAanbod());
+            verfpotVoorUpdate.setVerpakkingMogelijkheden(verfpot.getVerpakkingMogelijkheden());
+            verfpotVoorUpdate.setPrijs(verfpot.getPrijs());
+            verfpotRepository.save(verfpot);
+        }
+        return "redirect:/verfdetail/"+id;
     }
 }
